c6381ac84d1c9dadf2f0dbbb4798465a6c07d752
Paulo <pauloborges091@gmail.com>
2025-10-29 09:59:47 -0300

    projeto

--- data.py
+++ data.py
@@ -1,1 +1,7 @@
-# Arquivo com constantes
+URBAN_ROUTES_URL = 'https://cnt-31f2cb50-162c-4a7a-baed-abdd4d56b1b4.containerhub.tripleten-services.com?lng=pt'# Arquivo com constantes
+ADDRESS_FROM = 'East 2nd Street, 601'
+ADDRESS_TO = '1300 1st St'
+PHONE_NUMBER = '+1 123 123 12 12'
+CARD_NUMBER = '1234 5678 9100'
+CARD_CODE = '1111'
+MESSAGE_FOR_DRIVER = 'Pare no bar de sucos'
