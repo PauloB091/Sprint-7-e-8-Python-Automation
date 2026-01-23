af74668a87e5d7ced72135071daa8d88980e2259
Paulo <pauloborges091@gmail.com>
2026-01-22 21:29:28 -0300

    entrega

--- data.py
+++ data.py
@@ -1,4 +1,4 @@
-URBAN_ROUTES_URL = 'https://cnt-31f2cb50-162c-4a7a-baed-abdd4d56b1b4.containerhub.tripleten-services.com?lng=pt'# Arquivo com constantes
+URBAN_ROUTES_URL = 'https://cnt-6c65e3d4-8899-4776-9797-b02b1bf3f31b.containerhub.tripleten-services.com?lng=pt'# Arquivo com constantes
 ADDRESS_FROM = 'East 2nd Street, 601'
 ADDRESS_TO = '1300 1st St'
 PHONE_NUMBER = '+1 123 123 12 12'
